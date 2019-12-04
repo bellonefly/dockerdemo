9cf8fc76a55099843c5b5c35a05cfee3a6f49f1a
鐘義翔 <gary@richhk.net>
2019-12-04 18:04:36 +0800

    更新

---
+++ DockerDemo/FrontStage.API/Controllers/InfoController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FrontStage.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class InfoController : ControllerBase
+    {
+		public object GetAsync()
+		{
+			dynamic resultOutput = new
+			{
+				HostName = Dns.GetHostName(),
+				HostIP = Dns.GetHostEntry(Dns.GetHostName()).AddressList.FirstOrDefault(x => x.AddressFamily == AddressFamily.InterNetwork).ToString()
+			};
+
+			return resultOutput;
+		}
+	}
+}
